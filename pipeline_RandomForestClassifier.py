c331f04a798b95be83e61db45c778974002ae920
Devid Bruno <65268444+devid-bruno@users.noreply.github.com>
2023-05-31 14:54:11 -0300

    preenchendo valores nulos e refazendo metrica

--- pipeline_RandomForestClassifier.py
+++ pipeline_RandomForestClassifier.py
@@ -5,8 +5,8 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
+import numpy as np
 
-# Carregar os dados
 df = pd.read_csv('casos_coronavirus_2023_05_23_0.csv', delimiter=";")
 
 deletecolumnsdf = ["identificadorCaso",  "idRedcap", "idEsus", "idSivep", "classificacaoEstadoRedcap",
@@ -21,25 +21,24 @@ deletecolumnsdf = ["identificadorCaso",  "idRedcap", "idEsus", "idSivep", "class
                    'dataNotificacaoObito', 'cnesNotificacaoEsus', 'municipioNotificacaoEsus', 'tipoObitoMaterno',
                    "classificacaoFinalCasoSivep"]
 
-# Selecionar as colunas de interesse
 colunas_analise = ['profissionalSaudeEsus', 'estadoCaso', 'idadeCaso', 'faixaEtaria', 'resultadoFinalExame', 'tipoTesteExame', 'racaCor', 'tipoTesteEsus', 'obitoConfirmado', 'classificacaoObito', 'localObito', 'tipoLocalObito']
 df = df[colunas_analise]
 
-# Converter colunas booleanas para numéricas (0 e 1)
 df['profissionalSaudeEsus'] = df['profissionalSaudeEsus'].astype(int)
 df['obitoConfirmado'] = df['obitoConfirmado'].astype(int)
 
-# Remover linhas com valores ausentes
-df = df.dropna()
+# df = df.dropna()
+df[colunas_analise] = df[colunas_analise].fillna('1')
 
-# Separar os dados em características (X) e variável-alvo (y)
+# # count_null_values = df.isna().sum()
+
+# # print(count_null_values)
+# print(df.dtypes)
 X = df.drop('obitoConfirmado', axis=1)
 y = df['obitoConfirmado']
 
-# Dividir os dados em conjuntos de treinamento e teste
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Pré-processamento dos dados
 numeric_features = ['idadeCaso']
 categorical_features = ['estadoCaso', 'faixaEtaria', 'resultadoFinalExame', 'tipoTesteExame', 'racaCor', 'tipoTesteEsus', 'classificacaoObito', 'localObito', 'tipoLocalObito']
 
@@ -52,16 +51,12 @@ preprocessor = ColumnTransformer(
         ('cat', categorical_transformer, categorical_features)
     ])
 
-# Criar o pipeline
 pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                            ('classifier', RandomForestClassifier())])
 
-# Treinar o modelo
 pipeline.fit(X_train, y_train)
 
-# Realizar a previsão
 y_pred = pipeline.predict(X_test)
 
-# Exibir o relatório de classificação
 report = classification_report(y_test, y_pred)
 print(report)
